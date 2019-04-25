0c90b432ead308c3d12166712bc484166f1ed016
josepplloo <josepplloo@gmail.com>
2019-04-25 13:34:52 -0500

    Understanding render prop components

--- src/exercises/06.js
+++ src/exercises/06.js
@@ -13,7 +13,7 @@ import {Switch} from '../switch'
 // with the ones we need to get our toggle functionality to work
 //
 // 💰 Here's a little utility that might come in handy
-// const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
+const callAll = (...fns) => (...args) => fns.forEach(fn => fn && fn(...args))
 
 class Toggle extends React.Component {
   state = {on: false}
@@ -22,6 +22,11 @@ class Toggle extends React.Component {
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+  getTogglerProps = ({onClick, ...props}) => ({
+    onClick: callAll(this.toggle, onClick),
+    'aria-expanded': this.state.on,
+    ...props,
+  })  
   getStateAndHelpers() {
     return {
       on: this.state.on,
@@ -30,6 +35,7 @@ class Toggle extends React.Component {
         'aria-pressed': this.state.on,
         onClick: this.toggle,
       },
+      getTogglerProps: this.getTogglerProps,
     }
   }
   render() {
